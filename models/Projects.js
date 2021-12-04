bf828fe8b515cfa684929c47b8c77ef26d06df0d
AranBeitia <arantza.beitia@gmail.com>
2021-12-04 19:59:35 +0100

    url

--- models/Projects.js
+++ models/Projects.js
@@ -1,14 +1,27 @@
 const Sequelize = require('sequelize')
 const db = require('../config/db')
+const slug = require('slug')
+const shortId = require('shortid')
 
-const Projects = db.define('projects', {
-	id: {
-		type: Sequelize.INTEGER,
-		primaryKey: true,
-		autoIncrement: true,
+const Projects = db.define(
+	'projects',
+	{
+		id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: Sequelize.STRING,
+		url: Sequelize.STRING,
 	},
-	name: Sequelize.STRING,
-	url: Sequelize.STRING,
-})
+	{
+		hooks: {
+			beforeCreate(project) {
+				const url = slug(project.name).toLowerCase()
+				project.url = `${url}-${shortId.generate()}`
+			},
+		},
+	}
+)
 
 module.exports = Projects
